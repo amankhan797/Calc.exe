004caf6c6e61d4d3a7dbf735ff8a3ae400e9be90
Aman Khan <amankhanaezakmi@gmail.com>
2023-06-04 17:45:42 +0530

    Tried to make an Calculator webpage when I was an beginner.

---
+++ function f1(){.js
@@ -0,0 +1,15 @@
+function f1(){
+    var a=parseInt(document.getElementById("a").value);
+    var b=parseInt(document.getElementById("b").value);
+    var c;
+    c=a+b;
+    document.getElementById("c").innerHTML=c;
+}
+/*code for subtracting*/
+function f2(){
+    var d=parseInt(document.getElementById("g").value);
+    var e=parseInt(document.getElementById("f").value);
+    var f;
+    f=d-e;
+    document.getElementById("h").innerHTML=f;
+}
---
+++ second_js.js
@@ -0,0 +1,24 @@
+function f1(){
+    var a=parseInt(document.getElementById("a").value);
+    var b=parseInt(document.getElementById("b").value);
+    var c;
+    c=a+b;
+    document.getElementById("c").innerHTML=c;   
+}
+
+/*code for subtracting*/
+
+function f2(){
+    var d=parseInt(document.getElementById("d").value);
+    var f=parseInt(document.getElementById("f").value);
+    var g;
+    g=d-f;
+    document.getElementById("g").innerHTML=g;   
+}
+function f3(){
+    var h=parseInt(document.getElementById("h").value);
+    var i=parseInt(document.getElementById("i").value);
+    var j;
+    j=i/h;
+    document.getElementById("j").innerHTML=j;   
+}
